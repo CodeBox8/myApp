168fa086869c016a76c02e4ded43708069d988b9
SanketPolekar <polekarsanket@gmail.com>
2025-02-09 00:44:03 +0530

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React, { createContext,Suspense  } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import NavBar from "./NavCom";
 import Home from "./componants/HomeComp";
